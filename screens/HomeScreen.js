99d158edc6acdc97bf069a9ebb03512275c35870
dmquinn <danielmurphyberling@gmail.com>
2023-01-25 10:06:27 +0100

    added geolocation to address

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,10 +12,14 @@ import {
   ChevronDownIcon,
   UserIcon,
   MagnifyingGlassIcon,
-  AdjustmentsIcon,
+  MapPinIcon,
 } from "react-native-heroicons/outline";
+// import { Location, Permissions } from "expo";
+import * as Location from "expo-location";
 import Categories from "../components/Categories";
 import FeatureRow from "../components/FeatureRow";
+import RestaurantCard from "../components/RestaurantCard";
+
 import sanityClient from "../sanity";
 
 export default function HomeScreen() {
@@ -23,6 +27,21 @@ export default function HomeScreen() {
   const [featuredCategories, setFeaturedCategories] = useState([]);
   const [restaurants, setRestaurants] = useState([]);
   const [query, setQuery] = useState("");
+  const [location, setLocation] = useState(undefined);
+
+  useEffect(() => {
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("please grant location permission");
+        return;
+      }
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      let address = await Location.reverseGeocodeAsync(currentLocation.coords);
+      setLocation(address[0]);
+    };
+    getPermissions();
+  }, []);
 
   useEffect(() => {
     sanityClient
@@ -45,7 +64,7 @@ export default function HomeScreen() {
       .fetch(
         `
       *[_type == "restaurant"] {
-        ...,        
+        ...,
       }`
       )
       .then((data) => {
@@ -54,7 +73,6 @@ export default function HomeScreen() {
   }, []);
 
   useEffect(() => {
-    console.log(query);
     if (restaurants) {
       const filtered = restaurants.filter((item) => {
         return item.name.toLowerCase().includes(query.toLowerCase());
@@ -75,15 +93,15 @@ export default function HomeScreen() {
         <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
           <Text className="font-bold text-xl">
-            Current Location
-            {/* <ChevronDownIcon size={20} color="#00CCBB" /> */}
+            {location?.street} {location?.streetNumber} {location?.city}{" "}
+            <MapPinIcon size={30} color="#00CCBB" />
           </Text>
         </View>
         {/* <UserIcon size={35} color="#00CCBB" /> */}
       </View>
       {/* search */}
       <View className="flex-row item-center space-x-2 pb-2 mx-4">
-        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+        <View className="flex-row space-x-2 flex-1 bg-gray-100 p-3">
           <MagnifyingGlassIcon color="gray" size={30} />
           <TextInput
             placeholder="Restaurants and cuisines"
@@ -102,14 +120,31 @@ export default function HomeScreen() {
           paddingBottom: 100,
         }}
       >
-        {/* Catagary */}
-        <Categories />
-        {/* {query &&
-          restaurants.map((restaurant) => {
-            <Text>{restaurant.name}</Text>;
-          })} */}
-        {
-          !featuredCategories?.map((category) => (
+        {!query && <Categories />}
+        <View className="flex-1 items-center mt-4">
+          {query &&
+            restaurants.map((restaurant) => {
+              return (
+                <RestaurantCard
+                  key={restaurant._id}
+                  id={restaurant._id}
+                  imgUrl={restaurant.image}
+                  title={restaurant.name}
+                  rating={restaurant.rating}
+                  genre={restaurant.type?.name}
+                  address={restaurant.address}
+                  short_description={restaurant.short_description}
+                  dishes={restaurant.dishes}
+                  long={restaurant.long}
+                  lat={restaurant.lat}
+                >
+                  {restaurant.name}
+                </RestaurantCard>
+              );
+            })}
+        </View>
+        {!query &&
+          featuredCategories?.map((category) => (
             <FeatureRow
               key={category._id}
               id={category._id}
@@ -117,8 +152,7 @@ export default function HomeScreen() {
               description={category.short_description}
               query={query}
             />
-          ))
-        }
+          ))}
       </ScrollView>
     </SafeAreaView>
   );
